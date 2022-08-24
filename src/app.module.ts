fc3c78475acf968a8dbdbf2a6f2c5d83b332d4be
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-08-24 06:22:24 -0400

    [add] Responses

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,46 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AuthModule } from './auth/auth.module'
+import { DatabaseModule } from './database/database.module'
+import { TagsModule } from './modules/tags/tags.module'
+import { AuthorsModule } from './modules/authors/authors.module'
+import { BooksModule } from './modules/books/books.module'
+import { ConfigModule } from '@nestjs/config'
+import { EditorialsModule } from './modules/editorials/editorials.module'
+import * as Joi from 'joi'
+
+import config from './config'
+import { AwsModule } from './modules/aws/aws.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        AuthModule,
+        DatabaseModule,
+        TagsModule,
+        AuthorsModule,
+        BooksModule,
+        ConfigModule.forRoot({
+            envFilePath: '.env',
+            load: [config],
+            isGlobal: true,
+            validationSchema: Joi.object({
+                JWT_SECRET_KEY: Joi.string().required(),
+                MONGO_DB: Joi.string().required(),
+                MONGO_ROOT_USERNAME: Joi.string().required(),
+                MONGO_ROOT_PASSWORD: Joi.string().required(),
+                MONGO_PORT: Joi.number().required(),
+                MONGO_HOST: Joi.string().required(),
+                MONGO_CONNECTION: Joi.string().required(),
+                NODE_ENV: Joi.string().required(),
+                PORT: Joi.number().required(),
+                NATS_HOST: Joi.string().required(),
+                AWS_BUCKET: Joi.string().required(),
+                AWS_ACCESS_KEY_ID: Joi.string().required(),
+                AWS_SECRET_ACCESS_KEY: Joi.string().required(),
+                ELASTICSEARCH_NODE: Joi.string().required(),
+            }),
+        }),
+        EditorialsModule,
+        AwsModule,
+    ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'
+import { AppModule } from './app.module'
+import config from './config'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    // Config
+    const configService = config()
+    // App
+    const app = await NestFactory.create(AppModule)
+    // NATS Microservice
+    app.connectMicroservice<MicroserviceOptions>({
+        transport: Transport.NATS,
+        options: {
+            servers: [`nats://${configService.nats}:4222`],
+        },
+    })
+    await app.startAllMicroservices()
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: false,
+            transformOptions: {
+                enableImplicitConversion: true,
+            },
+        }),
+    )
+    app.enableCors({
+        origin: '*',
+        methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    })
+    await app.listen(3000)
 }
-bootstrap();
+bootstrap()
